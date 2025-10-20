9a57c26a3c3a89ee9632808f246893b7d9c4b9fe
mkGerber <michaelgerber2004@gmail.com>
2025-10-19 21:42:41 -0400

    lab 9

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -20,13 +20,20 @@ def index(request):
 
     num_books_with_word = Book.objects.filter(title__icontains='the').count()
 
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_books_with_word': num_books_with_word,
+        'num_visits': num_visits,
     }
+
+
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
